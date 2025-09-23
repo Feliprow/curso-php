3f84844528d1dd3ffaeb6260ea027a8099e1e5a9
Feliprow <felipeneresvieira@gmail.com>
2025-09-23 20:47:41 -0300

    Primeiro exercício em php

---
+++ ex000/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        // tudo que for escrito dentro dessas chaves são escritas php
+
+            echo "Olá Mundo! \u{1F30E}";
+        ?>
+        
+    </h1>
+    <p>Se livrando da maldição!</p>
+    
+</body>
+</html>
